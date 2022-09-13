71ee2ad0907c513276bb72d034fc190c85e8915f
mecows <abdoulmecows@yahoo.com>
2022-09-12 21:13:56 -0700

    c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main -Entry Point
+ * 
+ * Return: Always o (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0):
+}
+ 
