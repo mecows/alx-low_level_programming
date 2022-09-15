d4b646d409de16b0d16f754b8812e1e9b25486fd
mecows <abdoulmecows@yahoo.com>
2022-09-14 21:52:01 -0700

    another one

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ *
+ * @n: input as integer
+ *
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	int 1;
+
+	1 = n % 10;
+	if (1 < 0)
+	{
+		_putchar(-1 + 48);
+		return (-1);
+	}
+	else
+	{
+		_putchar(1 + 48);
+		return (1);
+	}
+}
