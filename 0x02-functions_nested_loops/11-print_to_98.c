bcc3f90e9080c0d2306af041e619e8b1cd363d73
mecows <abdoulmecows@yahoo.com>
2022-09-14 23:50:45 -0700

    new test6

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,19 +14,19 @@ int print_to_98(int n)
 	{
 		for (; n > 98; n--)
 		{
-			printf("%d, ", "98 ", n);
+			printf("%d, ", n);
 		}
 	}
 	else if (n < 98)
 	{
 		for (; n < 98; n++)
 		{
-			printf("%d, ", "98 ", n);
+			printf("%d, ", n);
 		}
 	}
 	else
 	{
-		printf("%d ", n);
+		printf("%d\n", n);
 	}
 	return (0);
 }
