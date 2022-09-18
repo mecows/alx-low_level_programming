d3fc9d1090409367ddd5f1eb6c6d8bca94965f8b
mecows <abdoulmecows@yahoo.com>
2022-09-17 18:57:27 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,21 +8,21 @@
  *
  * Return: no return.
  */
-int print_to_98(int n)
+void print_to_98(int n)
 {
 	if (n > 98)
 	{
 		for (; n > 98; n--)
-			printf("%d, ", '98', n);
+		{
+			printf("%d, ", n);
+		}
 	}
 	else if (n < 98)
 	{
 		for (; n < 98; n++)
-			printf("%d, ", '98', n);
+		{
+			printf("%d, ", n);
+		}
 	}
-	else
-	{
-		printf("%d\n", n);
-	}
-	return (0);
+	printf("%d\n", n);
 }
